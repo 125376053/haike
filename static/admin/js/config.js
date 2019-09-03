ae9b7c8d0863767157635a5ac4b9d9bae9944ae9
zhang <125376053@qq.com>
2019-09-03 16:45:13 +0800

    后台

--- static/admin/js/config.js
+++ static/admin/js/config.js
@@ -17,7 +17,8 @@ var config = {
 }
 
 
-var api = 'http://47.56.120.18:8080' //远程
+var api = "http://zhangfuming.cn"
+//var api = 'http://47.56.120.18:8080' //远程
 //var api = 'http://192.168.101.83/haike' //本地
 //  /common/upload/img
 
